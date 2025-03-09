51820bbf26fb8f198aa340698e766ee502ffd64d
Mikhail Akhremenko <mikhail.akhremenko@dscim.com>
2025-03-09 21:07:46 +0300

    hometask3 db

--- main.py
+++ main.py
@@ -1,143 +1,40 @@
-import pandas as pd
 import numpy as np
-from typing import Union
 
-df = pd.read_csv('/md2_python_for_DA/hometasks/final_project/real_estate_data.csv', sep='\t', encoding='utf-8')
+from decimal import Decimal, getcontext, ROUND_UP, ROUND_HALF_UP
 
-def remove_outliers(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
-    q1 = df[column_name].quantile(0.25)
-    q3 = df[column_name].quantile(0.75)
-    iqr = q3 - q1
-
-    df = df[
-        df[column_name].between(
-            q1 - 1.5 * iqr,
-            q3 + 1.5 * iqr,
-        )
-    ]
-    return df
+import math
 
 
-def fill_nulls(df: pd.DataFrame) -> pd.DataFrame:
-    df['first_day_exposition'] = pd.to_datetime(df['first_day_exposition']).dt.normalize()
-    df['balcony'] = df['balcony'].fillna(0)
-    df['ponds_nearest'] = df['ponds_nearest'].fillna(0)
-    df['is_apartment'] = df['is_apartment'].astype(bool).fillna(False)
-    df['days_exposition'] = df['days_exposition'].fillna(0)
-    return df
+# defining round_half_up
+
+def round2(number: int | float, precision: int = 0) -> float:
+    from math import floor
+    multiplier = 10 ** precision
+    return floor(number * multiplier + 0.5) / multiplier
 
 
-def make_it_dope(df: pd.DataFrame, var: str, cols: Union[str, list]) -> pd.DataFrame:
-    var_avg_name = var + "_avg"
-    var_avg_total_value = df[var].mean()
+# def round2(number: float, precision: int = 0) -> float:
+#     from decimal import Decimal
+#     a = Decimal(number)
+#     a = a.quantize(Decimal(f"1.{'0' * precision}"))
+#     return a
 
-    if var_avg_name in df.columns:
-        df.drop(columns=[var_avg_name], inplace=True)
+b = round2(6.45, 1)
 
-    if var == 'ceiling_height':
-        df[var] = df[var].apply(lambda x: x - 10 if 11 < x <= 20 else x)
-        df[var] = df[var].apply(lambda x: round(x / 10, 2) if 20 < x <= 100 else x)
-        df[var] = df[var].apply(lambda x: round(x / 100, 2) if x > 100 else x)
+print(b)
 
-    df_modify = (
-        df
-        .groupby(cols)[var]
-        .mean()
-        .sort_values(ascending=False)
-        .reset_index()
-        .round(2)
-    )
-    df_modify.rename(columns={var: var_avg_name}, inplace=True)
-    df = df.merge(df_modify, on=cols, how='left')
+# a = 6.50001
+# def round2()
+# b = np.where(a - np.floor(a) < 0.5, np.floor(a), np.ceil(a))
+# print(b)
 
-    df[var] = df[var].fillna(df[var_avg_name])
-    df[var] = df[var].fillna(var_avg_total_value)
-    df.drop(columns=[var_avg_name], inplace=True)
+# rnd = lambda v, p=0: round(v * (10 ** p)) / (10 ** p)
 
-    return df
+# a = 6.5
+# b = Decimal(a).to_integral_value(rounding=ROUND_HALF_UP, )
+# print(b)
 
-df = make_it_dope(df, 'rooms', ['locality_name'])
-df = make_it_dope(df, 'ceiling_height', ['locality_name'])
-df = make_it_dope(df, 'living_area', ['locality_name', 'rooms', 'ceiling_height'])
-df = make_it_dope(df, 'total_area', ['locality_name', 'rooms', 'ceiling_height', 'living_area'])
-df = make_it_dope(df, 'kitchen_area', ['locality_name', 'rooms', 'ceiling_height', 'living_area', 'total_area'])
-
-
-
-
-
-def add_m2_price(df: pd.DataFrame) -> pd.DataFrame:
-    df['price_per_m2'] = (df['last_price'] / df['total_area']).round(2)
-    return df
-
-
-def create_bins(series, bins, labels):
-    return pd.cut(series, bins=bins, labels=labels, include_lowest=True)
-
-
-def make_bins(df: pd.DataFrame) -> pd.DataFrame:
-    df['cityCenters_nearest'] = df['cityCenters_nearest'].fillna(df['cityCenters_nearest'].max())
-    df['parks_nearest'] = df['parks_nearest'].fillna(df['parks_nearest'].max())
-    df['floor'] = df['floor'].fillna(df['floor'].min())
-    df['living_area'] = df['living_area'].fillna(df['living_area'].mean())
-
-    city_center_bin = {
-        'bins': [0, 1000, 3000, 5000, 10000, 20000, float('inf')],
-        'labels': ['0-1km', '1-3km', '3-5km', '5-10km', '10-20km', '20+km'],
-    }
-    park_nearest_bin = {
-        'bins': [0, 100, 500, 1000, 2000, float('inf')],
-        'labels': ['0-100', '101-500', '501-1000', '1001-2000', '2000+'],
-    }
-    floor_bin = {
-        'bins': [0, 1, 5, 10, 20, float('inf')],
-        'labels': ['1', '2-5', '6-10', '11-20', '21+'],
-    }
-    living_area_bin = {
-        'bins': [0, 30, 50, 70, 90, 150, float('inf')],
-        'labels': ['0-30', '31-50', '51-70', '71-90', '91-150', '151+'],
-    }
-
-    def create_bins(series, bins, labels):
-        return pd.cut(series, bins=bins, labels=labels, right=False)
-
-    df['cityCenters_range'] = create_bins(df['cityCenters_nearest'], **city_center_bin)
-    df['floor_range'] = create_bins(df['floor'], **floor_bin)
-    df['living_area_range'] = create_bins(df['living_area'], **living_area_bin)
-    df['parks_nearest_range'] = create_bins(df['parks_nearest'], **park_nearest_bin)
-
-    return df
-
-
-def get_fair_price(df: pd.DataFrame, var: str, cols: Union[str, list]) -> pd.DataFrame:
-    fair_price = 'fair_price'
-
-    if fair_price in df.columns:
-        df.drop(columns=[fair_price], inplace=True)
-
-    df_modify = (
-        df
-        .groupby(cols, observed=True)[var]
-        .mean()
-        #.round(2)
-        .reset_index()
-    )
-    df_modify.rename(columns={var: fair_price}, inplace=True)
-    df_modify[fair_price] = df_modify[fair_price].astype(float)
-
-    df = df.merge(df_modify, on=cols, how='left')
-    if var == 'price_per_m2':
-        df[fair_price] = df[fair_price] * df['price_per_m2']
-
-    return df
-
-
-group_by = [
-    'locality_name',
-    'cityCenters_range',
-    'floor_range',
-    'living_area_range',
-    'parks_nearest_range',
-]
-
-df = get_fair_price(df, 'price_per_m2', group_by)
+# getcontext().rounding = ROUND_UP
+# # print(getcontext())
+# b = round(Decimal(6.5))
+# print(b)
---
+++ md3_databases/hw3.sql
@@ -0,0 +1,403 @@
+-- 1.
+
+with wt_dir_tz_and_tm as (
+    select
+         a.departure_airport || ' - ' || a.arrival_airport  as direction
+        ,(a.actual_departure at time zone coalesce(b.timezone, 'utc'))::time  as tm
+    from
+        flights  a
+        left join airports_data  b
+            on b.airport_code = a.departure_airport
+    where 1=1
+        and a.actual_departure is not null
+        and a.actual_departure at time zone coalesce(b.timezone, 'utc') >= date'2017-03-01'
+        and a.actual_departure at time zone coalesce(b.timezone, 'utc') <  date'2017-04-01'
+)
+
+,wt_dir_fli_cnt as (
+    select
+         direction
+        ,sum(case when tm >= '21:00:00' or  tm <= '1:00:00' then 1 else 0 end)  as night_fli_cnt
+        ,sum(case when tm >= '6:00:00'  and tm <= '9:00:00' then 1 else 0 end)  as day_fli_cnt
+    from
+        wt_dir_tz_and_tm
+    where 1=1
+    group by
+        direction
+)
+
+,wt_rank as (
+    select
+         direction
+        ,night_fli_cnt
+        ,day_fli_cnt
+        ,dense_rank() over (order by night_fli_cnt desc)  as night_rn
+        ,dense_rank() over (order by day_fli_cnt   desc)  as day_rn
+    from
+        wt_dir_fli_cnt
+)
+
+select
+    -- there can be several champions, that is why I use rank and string_agg
+     sum(night_fli_cnt)                                         as night_flight_cnt
+    ,string_agg(direction, ' :: ') filter (where night_rn = 1)  as night_champion_directions
+    ,sum(day_fli_cnt)                                           as day_flight_cnt
+    ,string_agg(direction, ' :: ') filter (where day_rn = 1)    as day_champion_directions
+    ,current_timestamp
+from
+    wt_rank
+where 1=1
+
+
+
+
+-- 2
+
+with wt_msc_tz as (
+    select
+        timezone  as tz
+    from
+        airports_data
+    where 1=1
+        and city ->> 'ru' = 'Москва'
+        and airport_code = 'DME'
+    limit 1
+)
+
+,wt_departure as (
+    -- the subselects are faster here than join and filter
+    select
+         a.flight_id
+        ,(a.actual_departure at time zone (select tz from wt_msc_tz))::date  as departure_dt
+        --,(a.actual_departure at time zone b.tz)::date  as departure_dt
+    from
+        flights  a
+        --join wt_msc_tz  b
+        --    on  a.actual_departure at time zone b.tz >= date'2017-03-01'
+        --    and a.actual_departure at time zone b.tz <  date'2017-04-01'
+    where 1=1
+        and a.actual_departure at time zone (select tz from wt_msc_tz) >= date'2017-03-01'
+        and a.actual_departure at time zone (select tz from wt_msc_tz) <  date'2017-04-01'
+)
+
+select
+     a.departure_dt
+    ,b.fare_conditions
+    ,sum(b.amount)  as amt
+    ,current_timestamp
+from
+    wt_departure  a
+
+    join ticket_flights  b
+        on b.flight_id = a.flight_id
+where 1=1
+group by
+     a.departure_dt
+    ,b.fare_conditions
+order by
+     a.departure_dt
+    ,b.fare_conditions
+
+
+
+
+-- 3
+
+with wt_subcat as (
+    select
+         a.salesorderid
+        ,a.productid
+        ,c.productsubcategoryid
+        ,c.name  as subcat_name
+
+        ,count(1) over (partition by a.salesorderid, c.name)  as sub_cnt
+        ,sum(case when c.name = 'Wheels' then 1 else 0 end) over (partition by a.salesorderid)  as wheel_cnt
+    from
+        salesorderdetail  a
+
+        join product  b
+            on b.productid = a.productid
+
+        join productsubcategory  c
+            on c.productsubcategoryid = b.productsubcategoryid
+    where 1=1
+--        and a.salesorderid = 46932
+)
+
+
+,wt_rank as (
+    select
+         productsubcategoryid
+        ,subcat_name
+        ,count(distinct salesorderid)  as order_cnt
+        ,dense_rank() over (order by count(distinct salesorderid) desc)  as rank1
+    from
+        wt_subcat
+    where 1=1
+    group by
+         productsubcategoryid
+        ,subcat_name
+)
+
+select
+     productsubcategoryid
+    ,subcat_name
+    ,order_cnt
+    ,current_timestamp
+from
+    wt_rank
+where 1=1
+    and rank1 <= 5
+order by
+    rank1
+
+
+
+-- 4
+select
+     left(name, 1)  as first_letter
+    ,count(1)
+    ,current_timestamp
+from
+    country  a
+group by
+    left(name, 1)
+order by
+    left(name, 1)
+
+
+
+
+-- 5
+
+select
+     count(distinct a.name)  as country_cnt
+    ,current_timestamp
+from
+    country  a
+    join countrylanguage  b
+        on  b.countrycode = a.code
+        and b.isofficial is true
+        and b.language in ('English', 'German')
+where 1=1
+
+
+
+-- 6
+
+with wt_avg_le as (
+    select
+         region
+        ,name  as country_name
+        ,lifeexpectancy
+        ,avg(lifeexpectancy) over (partition by region)  as avg_reg_le
+    from
+        country
+    where 1=1
+)
+
+select
+     region
+    ,country_name
+    ,lifeexpectancy
+    ,current_timestamp
+from
+    wt_avg_le
+where 1=1
+    and lifeexpectancy > avg_reg_le
+
+
+-- 7
+
+select distinct
+     a.language
+    ,current_timestamp
+from
+    countrylanguage  a
+where 1=1
+    and a.countrycode in (
+        select
+            b.code
+        from
+            country  b
+        where 1=1
+            and b.continent in ('Asia', 'Africa')
+    )
+order by
+    a.language
+
+
+
+-- 8
+
+with wt_raw as (
+    select
+         to_char(sh.orderdate, 'Month')  as month_name
+        ,to_char(sh.orderdate, 'mm')     as mm
+        ,to_char(sh.orderdate, 'yyyy')   as yyyy
+
+        ,c.name  as cat_name
+        ,a.linetotal
+    from
+        salesorderdetail  a
+
+        join salesorderheader  sh
+            on  sh.salesorderid = a.salesorderid
+            and sh.orderdate >= date'2012-01-01'
+            and sh.orderdate <  date'2014-01-01'
+
+        join product  b
+            on b.productid = a.productid
+
+        join productsubcategory  sc
+            on sc.productsubcategoryid = b.productsubcategoryid
+
+        join productcategory  c
+            on c.productcategoryid = sc.productcategoryid
+    where 1=1
+)
+
+,wt_year_by_col as (
+    select
+         month_name
+        ,mm
+        ,cat_name
+
+        ,sum(case when yyyy = '2012' then linetotal else 0 end)  as linetotal_2012
+        ,sum(case when yyyy = '2013' then linetotal else 0 end)  as linetotal_2013
+    from
+        wt_raw
+    where 1=1
+    group by
+         month_name
+        ,cat_name
+        ,mm
+)
+
+select
+     month_name
+    ,cat_name
+    ,linetotal_2012
+    ,linetotal_2013
+
+    ,round(
+        case
+            when linetotal_2012 = 0
+                then
+                    1
+            else
+                (linetotal_2013 - linetotal_2012) / linetotal_2012 * 100
+        end
+        ,2
+    )  as diff_in_prc
+
+    ,current_timestamp
+from
+    wt_year_by_col
+where 1=1
+order by
+     mm
+    ,cat_name
+
+
+
+-- 9
+
+with wt_par as materialized (
+    select
+         date'2017-03-01'  as from_dt
+        ,date'2017-03-31'  as to_dt
+        ,(
+            select
+                timezone  as tz
+            from
+                airports_data
+            where 1=1
+                and city ->> 'ru' = 'Москва'
+                and airport_code = 'DME'
+            limit 1
+        )  as msc_tz
+)
+
+
+,wt_departure_and_arrival as (
+    select
+         a.flight_id
+
+        ,a.departure_airport                                 as airport
+        ,'departure'                                         as typ
+        ,(a.actual_departure at time zone pa1.msc_tz)::date  as dt
+    from
+        flights  a
+        join wt_par  pa1
+            on  a.actual_departure at time zone pa1.msc_tz >= pa1.from_dt
+            and a.actual_departure at time zone pa1.msc_tz <  pa1.to_dt + 1
+    where 1=1
+
+    union all
+
+    select
+         a.flight_id
+
+        ,a.arrival_airport
+        ,'arrival'  as typ
+        ,(a.actual_arrival at time zone pa1.msc_tz)::date  as dt
+    from
+        flights  a
+        join wt_par  pa1
+            on  a.actual_arrival at time zone pa1.msc_tz >= pa1.from_dt
+            and a.actual_arrival at time zone pa1.msc_tz <  pa1.to_dt + 1
+    where 1=1
+)
+
+
+,wt_ticket_cnt as (
+    select
+         flight_id
+        ,count(1)  as ticket_cnt
+    from
+        ticket_flights
+    where 1=1
+        and flight_id in (select flight_id from wt_departure_and_arrival)
+    group by
+        flight_id
+)
+
+
+,wt_rank as (
+    select
+         coalesce(b.airport_name ->> 'ru', a.airport)  as airport_name_ru
+
+        ,sum(case when a.typ = 'departure' then 1 else 0 end)  as departure_cnt
+        ,sum(case when a.typ = 'arrival'   then 1 else 0 end)  as arrival_cnt
+        ,count(1)                                              as flight_cnt
+        ,sum(coalesce(c.ticket_cnt, 0))                        as ticket_cnt
+
+        ,dense_rank() over (order by count(1) desc)  as rank1
+    from
+        wt_departure_and_arrival  a
+
+        left join airports_data  b
+            on b.airport_code = a.airport
+
+        left join wt_ticket_cnt  c
+            on c.flight_id = a.flight_id
+    where 1=1
+    group by
+         coalesce(b.airport_name ->> 'ru', a.airport)
+)
+
+select
+     airport_name_ru
+
+    ,departure_cnt
+    ,arrival_cnt
+    ,flight_cnt
+    ,ticket_cnt
+
+    ,current_timestamp
+from
+    wt_rank
+where 1=1
+    and rank1 <= 10
+
